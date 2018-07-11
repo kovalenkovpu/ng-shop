5e5d83877b328fa53f6fde4f9fe06c01e2c1463b
Pavel Kovalenkov <Pavel_Kovalenkov@epam.com>
2018-07-11 19:02:10 +0300

    Changes to Task2 (shared module)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SharedModuleModule } from './shared-module/shared-module.module';
+// import { SharedModuleModule } from './shared-module/shared-module.module';
 import { CartModule } from './cart-module/cart.module';
 import { ProductsModule } from './products-module/products.module';
 
@@ -12,7 +12,6 @@ import { ProductsModule } from './products-module/products.module';
   ],
   imports: [
       BrowserModule,
-      SharedModuleModule,
       ProductsModule,
       CartModule,
   ],
--- src/app/cart-module/cart.module.ts
+++ src/app/cart-module/cart.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { CartComponent } from './cart.component';
 import { CartListComponent } from './cart-list/cart-list.component';
 import { CartItemComponent } from './cart-item/cart-item.component';
+import {SharedModuleModule} from '../shared-module/shared-module.module';
 
 @NgModule({
   imports: [
-    CommonModule
+      CommonModule,
+      SharedModuleModule
   ],
     exports: [CartComponent],
     declarations: [CartComponent, CartListComponent, CartItemComponent]
